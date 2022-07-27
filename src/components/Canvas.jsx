81234431b6138117e6d2b8502c3eb755ee48a50a
Nikhil <makwananikhil36@gmail.com>
2022-07-27 22:09:42 +0530

    code refactor

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -44,29 +44,13 @@ function Canvas(props) {
         let delta = opt.e.deltaY;
         let zoom = canvas.getZoom();
         zoom *= 0.999 ** delta;
-        console.log(zoom);
 
         if (zoom > 20) zoom = 20;
         if (zoom < 1) zoom = 1;
         canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+
         opt.e.preventDefault();
         opt.e.stopPropagation();
-        let vpt = this.viewportTransform;
-        if (zoom < 400 / 1000) {
-          vpt[4] = 200 - (1000 * zoom) / 2;
-          vpt[5] = 200 - (1000 * zoom) / 2;
-        } else {
-          if (vpt[4] >= 0) {
-            vpt[4] = 0;
-          } else if (vpt[4] < canvas.getWidth() - img.width * zoom) {
-            vpt[4] = canvas.getWidth() - img.width * zoom;
-          }
-          if (vpt[5] >= 0) {
-            vpt[5] = 0;
-          } else if (vpt[5] < canvas.getHeight() - img.height * zoom) {
-            vpt[5] = canvas.getHeight() - img.height * zoom;
-          }
-        }
       });
 
       canvas.add(img);
